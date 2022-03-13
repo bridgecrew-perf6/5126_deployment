1467eab27ce18f8b1b690cef8006e1e9d18f86d0
Dheeraj Singh <dheeraj.student2@gmail.com>
2022-03-13 18:12:40 +0800

    Added initial app

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from multipage import MultiPage
 
+st.set_page_config(
+     page_title="HDB resale price analysis",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
+
+app = MultiPage()
